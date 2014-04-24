0e729b1241fd715ede4e6ba0a0fb26188c384c5b
KangWoo Lee <kangwool@usc.edu>
2014-04-23 22:10:47 -0700

    Cohen's Kappa
    Cohen's Kappa for intra-rater agreement

---
+++ Cohen_kappa.R
@@ -0,0 +1,62 @@
+# usage: source('Cohen_kappa.R',print.eval=TRUE)
+# This function reads 
+
+# Basic settings
+setwd("c:\\Code\\emg_R_analysis"); library(fmsb) # Cohen's kappa
+# Shouldn't be changed 
+rm(list = ls()); num_trials<-25; Vowel<-c('ɑ', 'ɔ', 'ɛ', 'i', 'u');Encoding(Vowel)<-'UTF-8'
+len_vowel<-length(Vowel)
+Cohen_BC<-matrix(0, nrow = len_vowel, ncol = len_vowel);
+Cohen_BP<-Cohen_BC; Cohen_FC<-Cohen_BC; Cohen_FP<-Cohen_BC 
+# colnames(Cohen_matrix)<-Vowel; rownames(Cohen_matrix)<-Vowel;
+## Can be changed
+cur_path<-"C:\\Users\\Labuser\\Documents\\Github\\keewii-visual\\data\\Healthy"
+listener<- "kw" ; log_filename<-paste(c('test-log-',listener,'.txt'),collapse='')
+#answer_col<-3 # col1:path, col2: right answer, col3: answer
+# list filenames of brac & fpb, odd index is cartesian, even index is polar coordinate
+dirs_brac<-grep("[C]+$",list.dirs(cur_path,recursive=FALSE),value=TRUE)
+filenames_brac<-list.files(dirs_brac,log_filename, recursive=TRUE, full.names=TRUE)
+dirs_fpb<-grep("[B]+$",list.dirs(cur_path,recursive=FALSE),value=TRUE)
+filenames_fpb<-list.files(dirs_fpb,log_filename, recursive=TRUE, full.names=TRUE) 
+numfile<-length(filenames_brac)/2 # number of subject
+temp_brac_cart<-matrix(numeric(0), 0,2) 
+temp_brac_polar<-matrix(numeric(0), 0,2) 
+temp_fpb_cart<-matrix(numeric(0), 0,2) 
+temp_fpb_polar<-matrix(numeric(0), 0,2) 
+
+for (a in 1:numfile){
+  temp<-read.table(filenames_brac[2*a-1],header=FALSE,nrows=num_trials,encoding='UTF-8')[,2:3]
+  temp_brac_cart<-rbind(temp_brac_cart,temp)
+  
+  temp<-read.table(filenames_brac[2*a],header=FALSE,nrows=num_trials,encoding='UTF-8')[,2:3]
+  temp_brac_polar<-rbind(temp_brac_polar,temp)
+  
+  temp<-read.table(filenames_fpb[2*a-1],header=FALSE,nrows=num_trials,encoding='UTF-8')[,2:3]
+  temp_fpb_cart<-rbind(temp_fpb_cart,temp)
+  
+  temp<-read.table(filenames_fpb[2*a],header=FALSE,nrows=num_trials,encoding='UTF-8') [,2:3] 
+  temp_fpb_polar<-rbind(temp_fpb_polar,temp)
+}
+#temp_brac_cart<-read.table(filenames_brac[3],header=FALSE,nrows=num_trials,encoding='UTF-8')[,2:3]
+N<-numfile*num_trials
+for (k in 1:N)
+for (a in 1:len_vowel){
+  for (b in 1:len_vowel){
+    if (temp_brac_cart[k,1]==Vowel[b] & temp_brac_cart[k,2]==Vowel[a]){
+      Cohen_BC[a,b]<-Cohen_BC[a,b]+1
+    }
+    if (temp_brac_polar[k,1]==Vowel[b] & temp_brac_polar[k,2]==Vowel[a]){
+      Cohen_BP[a,b]<-Cohen_BP[a,b]+1
+    }
+    if (temp_fpb_cart[k,1]==Vowel[b] & temp_fpb_cart[k,2]==Vowel[a]){
+      Cohen_FC[a,b]<-Cohen_FC[a,b]+1
+    }
+    if (temp_fpb_polar[k,1]==Vowel[b] & temp_fpb_polar[k,2]==Vowel[a]){
+      Cohen_FP[a,b]<-Cohen_FP[a,b]+1
+    }   
+  }
+}
+Kappa.test(Cohen_BC, y=NULL, conf.level=0.95)
+Kappa.test(Cohen_BP, y=NULL, conf.level=0.95)
+Kappa.test(Cohen_FC, y=NULL, conf.level=0.95)
+Kappa.test(Cohen_FP, y=NULL, conf.level=0.95)
